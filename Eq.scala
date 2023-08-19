a650e6f1a2f60b98b048542bc7090f98725c45c7
Martin Hansen <code@martin-hansen.de>
2023-08-19 09:27:00 +0200

    initial commit
    This is a direct copy of the example from
    https://github.com/lampepfl/dotty/commit/982ce3fc36535eb7cb96cf7d25694937b9005ced
    
    (Might be slightly reformatted + a missing import for AbstractIterable was
    added.)

---
+++ Eq.scala
@@ -0,0 +1,66 @@
+import scala.collection.mutable.AbstractIterable
+import scala.deriving.*
+import scala.compiletime.{error, erasedValue, summonInline}
+
+inline def summonInstances[T, Elems <: Tuple]: List[Eq[?]] =
+  inline erasedValue[Elems] match
+    case _: (elem *: elems) =>
+      deriveOrSummon[T, elem] :: summonInstances[T, elems]
+    case _: EmptyTuple => Nil
+
+inline def deriveOrSummon[T, Elem]: Eq[Elem] =
+  inline erasedValue[Elem] match
+    case _: T => deriveRec[T, Elem]
+    case _    => summonInline[Eq[Elem]]
+
+inline def deriveRec[T, Elem]: Eq[Elem] =
+  inline erasedValue[T] match
+    case _: Elem => error("infinite recursive derivation")
+    case _ =>
+      Eq.derived[Elem](using
+        summonInline[Mirror.Of[Elem]]
+      ) // recursive derivation
+
+trait Eq[T]:
+  def eqv(x: T, y: T): Boolean
+
+object Eq:
+  given Eq[Int] with
+    def eqv(x: Int, y: Int) = x == y
+
+  def check(x: Any, y: Any, elem: Eq[?]): Boolean =
+    elem.asInstanceOf[Eq[Any]].eqv(x, y)
+
+  def iterable[T](p: T): Iterable[Any] = new AbstractIterable[Any]:
+    def iterator: Iterator[Any] = p.asInstanceOf[Product].productIterator
+
+  def eqSum[T](s: Mirror.SumOf[T], elems: => List[Eq[?]]): Eq[T] =
+    new Eq[T]:
+      def eqv(x: T, y: T): Boolean =
+        val ordx = s.ordinal(x)
+        (s.ordinal(y) == ordx) && check(x, y, elems(ordx))
+
+  def eqProduct[T](p: Mirror.ProductOf[T], elems: => List[Eq[?]]): Eq[T] =
+    new Eq[T]:
+      def eqv(x: T, y: T): Boolean =
+        iterable(x).lazyZip(iterable(y)).lazyZip(elems).forall(check)
+
+  inline def derived[T](using m: Mirror.Of[T]): Eq[T] =
+    lazy val elemInstances = summonInstances[T, m.MirroredElemTypes]
+    inline m match
+      case s: Mirror.SumOf[T]     => eqSum(s, elemInstances)
+      case p: Mirror.ProductOf[T] => eqProduct(p, elemInstances)
+end Eq
+
+enum Lst[+T] derives Eq:
+  case Cns(t: T, ts: Lst[T])
+  case Nl
+
+extension [T](t: T) def ::(ts: Lst[T]): Lst[T] = Lst.Cns(t, ts)
+
+@main def test(): Unit =
+  import Lst.*
+  val eqoi = summon[Eq[Lst[Int]]]
+  assert(eqoi.eqv(23 :: 47 :: Nl, 23 :: 47 :: Nl))
+  assert(!eqoi.eqv(23 :: Nl, 7 :: Nl))
+  assert(!eqoi.eqv(23 :: Nl, Nl))
---
+++ README.md
@@ -0,0 +1,2 @@
+Experiments with recursive typeclass derivation in Scala 3, based on the [`Eq` example from
+the official documentation](https://docs.scala-lang.org/scala3/reference/contextual/derivation.html).
